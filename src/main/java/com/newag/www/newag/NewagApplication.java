997805a8bf05280ce2df6cc890a9c2bcae81c70c
Maik Tran van <m_tv@gmx.de>
2019-05-13 13:43:28 +0200

    initialize

---
+++ src/main/java/com/newag/www/newag/NewagApplication.java
@@ -0,0 +1,13 @@
+package com.newag.www.newag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewagApplication.class, args);
+	}
+
+}
