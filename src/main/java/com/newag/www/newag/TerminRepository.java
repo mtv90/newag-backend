d102debf8b74c5fe4f909dc6f327c9622382edda
Maik Tran van <m_tv@gmx.de>
2019-05-21 21:08:35 +0200

    changes in REST

--- src/main/java/com/newag/www/newag/TerminRepository.java
+++ src/main/java/com/newag/www/newag/TerminRepository.java
@@ -2,8 +2,10 @@ package com.newag.www.newag;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-@RepositoryRestResource
+@CrossOrigin
+@RepositoryRestResource(path="termine")
 public interface TerminRepository extends JpaRepository<Termin, Long>{
 	
 }
